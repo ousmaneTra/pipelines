b642d2d84e0d529a4aac4186d80c48ac96adef33
Yinguang Zhao <yinguang@google.com>
2022-05-17 17:20:06 -0700

    Adding ML.CONFUSION_MATRIX KFP BQ Component.
    PiperOrigin-RevId: 449353885

--- components/google-cloud/google_cloud_pipeline_components/experimental/bigquery/__init__.py
+++ components/google-cloud/google_cloud_pipeline_components/experimental/bigquery/__init__.py
@@ -26,6 +26,7 @@ __all__ = [
     'BigqueryExportModelJobOp',
     'BigqueryPredictModelJobOp',
     'BigqueryEvaluateModelJobOp',
+    'BigqueryMLConfusionMatrixJobOp',
 ]
 
 BigqueryQueryJobOp = load_component_from_file(
@@ -43,3 +44,6 @@ BigqueryPredictModelJobOp = load_component_from_file(
 
 BigqueryEvaluateModelJobOp = load_component_from_file(
         os.path.join(os.path.dirname(__file__), 'evaluate_model/component.yaml'))
+
+BigqueryFeatureInfoJobOp = load_component_from_file(
+        os.path.join(os.path.dirname(__file__), 'ml_confusion_matrix/component.yaml'))
